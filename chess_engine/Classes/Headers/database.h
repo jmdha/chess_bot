e1f09d0baa93f5755a19d6f9929bb0f25ebd0c4f
jamadaha <53914190+jamadaha@users.noreply.github.com>
2020-09-23 20:40:31 +0200

    Updated SQLite

--- chess_engine/Classes/Headers/database.h
+++ chess_engine/Classes/Headers/database.h
@@ -2,16 +2,19 @@
 #define Database_H
 #include "../../sqlite3.h"
 #include <string>
+#include <chrono>
+
 class Database
 {
 public:
+	sqlite3* db;
 	Database(const char* path);
 	void closeDatabase();
 	void addEntry(unsigned long hash, const char * move);
+	void printProgress();
 private:
-	sqlite3* db;
 	int rc;
-	
+	std::chrono::steady_clock::time_point startTime = std::chrono::high_resolution_clock::now();
 };
 
 #endif
