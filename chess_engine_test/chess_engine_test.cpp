1b9f321cd420bf6556a2fb692a3c65a144efa298
jamadaha <53914190+jamadaha@users.noreply.github.com>
2021-01-22 19:20:49 +0100

    Minor change to test

--- chess_engine_test/chess_engine_test.cpp
+++ chess_engine_test/chess_engine_test.cpp
@@ -18,7 +18,7 @@ namespace EngineTest
 			// tests blank squares
 			for (int y = 2; y < 6; y++)
 				for (int x = 0; x < 8; x++)
-					Assert::IsTrue(nullptr == board.getPiece(x, y));
+					Assert::IsTrue(board.isSquareEmpty(x, y));
 
 			// tests for pawn placement
 			for (int x = 0; x < 8; x++) {
@@ -53,7 +53,7 @@ namespace EngineTest
 			board.clearBoard();
 			for (int y = 0; y < 8; y++)
 				for (int x = 0; x < 8; x++)
-					Assert::IsTrue(nullptr == board.getPiece(x, y));
+					Assert::IsTrue(board.isSquareEmpty(x, y));
 		}
 
 		TEST_METHOD(GetPiece) {
@@ -61,6 +61,13 @@ namespace EngineTest
 			Assert::IsTrue(nullptr == board.getPiece(-1, -1));
 			Assert::IsTrue(nullptr == board.getPiece(4, 4));
 			Assert::IsTrue(nullptr != board.getPiece(0, 0));
+		}
+
+		TEST_METHOD(IsSquareEmpty) {
+			Board board = Board();
+			Assert::IsTrue(board.isSquareEmpty(-1, -1));
+			Assert::IsTrue(board.isSquareEmpty(4, 4));
+			Assert::IsTrue(!board.isSquareEmpty(0, 0));
 		}
 	};
 }
