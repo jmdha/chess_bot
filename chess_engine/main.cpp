58b73abf35ddfcaf4400dce9ff48ee0fbd4d9e66
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2021-06-16 01:47:35 +0200

    Removed debugging code

--- chess_engine/main.cpp
+++ chess_engine/main.cpp
@@ -8,16 +8,8 @@ int main(int argc, char *argv[])
 {
     Board board = Board();
 
-    //if (argc > 1)
-   //board.importFakePGN(argv[1]);
-
-    board.importFEN("1rb2r2/2bpn1kp/7p/1p3p1B/1Np1P3/P1P5/3K1QP1/5R2 b - - 1 35");
-    board.turn = BLACK;
-    for (int i = 0; i < 2; i++) {
-        for (int ii = 0; ii < 2; ii++) {
-            board.castlingValid[i][ii] = false;
-        }
-    }
+    if (argc > 1)
+        board.importFakePGN(argv[1]);
 
     Move move = getBestMove(&board, 3);
 
