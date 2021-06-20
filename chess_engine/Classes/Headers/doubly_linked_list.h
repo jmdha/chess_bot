c54fe572299c52635d75cf0e90285ea09fdd0f2e
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2021-06-21 00:20:39 +0200

    Added node to piece

---
+++ chess_engine/Classes/Headers/doubly_linked_list.h
@@ -0,0 +1,14 @@
+#ifndef DoublyLinkedList_H
+#define DoublyLinkedList_H
+
+#include "node.h"
+
+class DoublyLinkedList {
+public:
+	Node* first;
+	Node* last;
+};
+
+
+
+#endif
---
+++ chess_engine/Classes/Headers/node.h
@@ -0,0 +1,15 @@
+#ifndef Node_H
+#define Node_H
+
+#include "piece.h"
+
+class Node {
+public:
+	Piece* next;
+	Piece* prev;
+
+};
+
+
+
+#endif
