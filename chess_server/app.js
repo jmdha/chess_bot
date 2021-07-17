78b5855fcd2eff6d18c4877e0183df93e62444d0
Jan M. D. Hansen <53914190+jamadaha@users.noreply.github.com>
2021-07-17 15:04:43 +0200

    fix(Server): now uses dotenv for config
    - closes #18

--- chess_server/app.js
+++ chess_server/app.js
@@ -2,22 +2,17 @@ const path = require('path');
 const InstanceManager = require(path.resolve('InstanceManager'));
 const Api = require(path.resolve('Api'));
 const UI = require(path.resolve('UI'));
+const result = require('dotenv').config({ path: path.resolve('config.env') });
+// If the file doesn't exist ... or any other reason 
+if (result.error)
+    throw result.error;
 
-// used for reading id
-const fs = require('fs');
 
-let bearerID;
-let botID;
-readID();
-function readID() {
-    let ids = fs.readFileSync('id.txt', 'utf8').split('\n');
-    bearerID = ids[0].split(' ')[1].trim();
-    botID = ids[1].split(' ')[1].trim();
-}
+
 
 function lichessStream(res) {
     let ui = new UI();
-    let instanceManager = new InstanceManager(new Api(bearerID, botID), ui);
+    let instanceManager = new InstanceManager(new Api(process.env.BEARER_ID, process.env.BOT_ID), ui);
     res.on('data', function (chunk) {
         let data;
         try {
@@ -36,7 +31,7 @@ function start() {
     const https = require('https');
     https.get('https://lichess.org/api/stream/event', {
         headers: {
-            Authorization: `Bearer ${bearerID}`
+            Authorization: `Bearer ${process.env.BEARER_ID}`
         }
     }, lichessStream);
 }
